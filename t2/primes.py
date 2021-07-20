48167be7e54a4824bc9d748bc715e511b6318e8d
alek.frohlich <alek.frohlich@gmail.com>
2021-07-19 23:31:06 -0300

    BBS

--- t2/primes.py
+++ t2/primes.py
@@ -2,20 +2,6 @@
 import random
 
 
-def fast_exp(x, e, m):
-    X = x
-    E = e
-    Y = 1
-    while E > 0:
-        if E % 2 == 0:
-            X = (X * X) % m
-            E = E // 2
-        else:
-            Y = (X * Y) % m
-            E = E - 1
-    return Y
-
-
 def miller_rabin(n,k):
     """Probabilistic primality test as described in Wikipedia.
 
@@ -36,7 +22,6 @@ def miller_rabin(n,k):
     while (d % 2) == 0:
         d = d // 2
         s += 1
-    # print("n = {}*2^{}+1".format(d,s))
 
     # Perform the test k times
     for __ in range(k):
@@ -50,15 +35,15 @@ def miller_rabin(n,k):
         # Luckly, a number n is typically not a false witness to many different basis. Hence, we repeat the test
         #   multiple times to incrise our confidence in the primality of n.
 
+        # n=1 or n=n-1 trivially satisfy some of the congruences, so we exclude them.
         a = random.randint(2,n-2)
-        x = fast_exp(a,d,n)
-        # print("a={}".format(a)) ; print("x={}".format(x))
+        x = pow(a,d,n)
         if x == 1 or x == n-1:
             continue
 
         passed = False
         for _ in range(s-1):
-            x = fast_exp(x,2,n)
+            x = pow(x,2,n)
             if x == n-1:
                 passed = True
 
@@ -70,16 +55,12 @@ def miller_rabin(n,k):
     return True
 
 
-def jacobi(a, n):
-    """Computes the Jacobi Symbol (a/n), except that -1 is returned as n-1."""
-
-
-def solovay_strassen(n, k):
+def fermat(n, k):
     """Probabilistic primality test as described in Wikipedia.
 
     Refs
     ----
-        https://en.wikipedia.org/wiki/Solovay%E2%80%93Strassen_primality_test
+        https://en.wikipedia.org/wiki/Fermat_primality_test
     """
     # Easy cases
     if n == 1:
@@ -91,9 +72,21 @@ def solovay_strassen(n, k):
 
     # Perform the test k times
     for __ in range(k):
-        a = random.randint(2, n-1)
-        x = jacobi(a, n)
+        # If n is prime, then it will satisfy the following congruence (Fermat's little thm)
+        #
+        #   a^(n-1) = 1 (mod n), for all a s.t. 0 < a < n
+        #
+        # A composite may satisfy this congruence, making it a Fermat pseudo-prime. That is why we run the
+        # test multiple times. The goal is thus to find a witness to the compositeness of n; i.e., a nonzero
+        # a s.t. n does not satisfy the congruence. A result states that having fixed n, at least half of the
+        # numbers a in the range (0,n) which are coprime to n are witnesses (ignoring charmichel numbers).
+        # This means that the method gets worse the more composite n is.
 
-        if x == 0 or fast_exp(a, (n-1)//2, n) != x:
+        # n=1 or n=n-1 trivially satisfy the congruence, so we exclude them.
+        a = random.randint(2,n-2)
+        x = pow(a,n-1,n)
+        if x == 1:
+            continue
+        else: # Found a witness
             return False
     return True
--- t2/prng.py
+++ t2/prng.py
@@ -21,10 +21,38 @@ class BBS:
         self._state = seed
         self._n = n
 
+    def _next_int(self, bits):
+        integer = 0
+        for _ in range(bits):
+            self._state = pow(self._state, 2, self._n)
+            integer <<= 1
+            integer |= (self._state & 1)
+        return integer
+
+    def next(self, bits):
+        done = False
+        n = None
+        while not done:
+            n = self._next_int(bits)
+            if self._state >= 2**bits:
+                done = True
+        return n
+
+
+class ICG:
+    def __init__(self, seed, a, b, q):
+        self._state = seed
+        self._a = a
+        self._b = b
+        self._q = q
+
     def next(self, bits):
         done = False
         while not done:
-            self._state = (self._state * self._state) % self._n
+            if self._state == 0:
+                self._state = self._b
+            else:
+                self._state = (self._a*pow(self._state,-1,self._q) + self._b) % self._q
             if self._state >= 2**bits:
                 done = True
         return self._state
